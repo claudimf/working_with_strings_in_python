5a1a65409a18ed60d084cbd941f6b9ec7babb644
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-03-12 12:20:16 -0300

    Makes course class "O método find"

---
+++ src/aulas/02.py
@@ -0,0 +1,16 @@
+#Using find
+
+argumento = "moedaorigem=real"
+index = argumento.find('=')
+subString = argumento[index + 1:]
+print(subString)
+
+argumento = 'https://www.bytebank.com.br/cambio?moedaorigem=real'
+index = argumento.find('=')
+subString = argumento[index + 1:]
+print(subString)
+
+# Using split
+argumento = "moedaorigem=real"
+listargumento = argumento.split('=')
+print(listargumento)
